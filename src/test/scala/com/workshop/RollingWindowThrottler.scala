94ab6b1799360ef0a41a818ec0ae65bfe3d6ccaa
kfirb <kfirb@wix.com>
2016-03-11 21:50:58 +0200

    refactor production code

--- src/test/scala/com/workshop/RollingWindowThrottler.scala
+++ src/test/scala/com/workshop/RollingWindowThrottler.scala
@@ -12,15 +12,27 @@ class RollingWindowThrottler(durationWindow: FiniteDuration, max: Int, clock: Cl
   private val invocationInfo = collection.mutable.HashMap.empty[String, InvocationInfo]
 
   def tryAcquire(key: String): Boolean = {
-    val invocation = invocationInfo.getOrElseUpdate(key, new InvocationInfo(new AtomicLong(0), clock.millis))
-    if ((clock.millis() - invocation.timeStamp) > durationWindow.toMillis){
-      invocationInfo.put(key, new InvocationInfo(new AtomicLong(0), clock.millis()))
-      true
-    }
-    else
-      invocation.counter.incrementAndGet() <= max
-
+    val invocation = invocationInfo.getOrElseUpdate(key, newInvocationInfo)
+    acquire((key, invocation))
   }
+
+  def acquire = reset orElse validate
+
+  private def reset: PartialFunction[(String, InvocationInfo), Boolean] = {
+    case (key, invocation) if expires(invocation) => {
+      invocationInfo.-=(key)
+      tryAcquire(key)
+    }
+  }
+
+  private def validate: PartialFunction[(String, InvocationInfo), Boolean] = {
+    case(key, invocation) => invocation.counter.incrementAndGet() <= max
+  }
+
+  private def newInvocationInfo = new InvocationInfo(counter = new AtomicLong(0),
+    clock.millis())
+
+  private def expires(invocation: InvocationInfo) = (clock.millis() - invocation.timeStamp) > durationWindow.toMillis
 }
 
 case class InvocationInfo(counter: AtomicLong, timeStamp: Long)

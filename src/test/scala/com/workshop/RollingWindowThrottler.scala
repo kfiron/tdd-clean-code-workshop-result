590787571a4d7d05166a907ec19d3a9268eef969
kfirb <kfirb@wix.com>
2016-03-11 21:16:52 +0200

    third test, change to map of keys

--- src/test/scala/com/workshop/RollingWindowThrottler.scala
+++ src/test/scala/com/workshop/RollingWindowThrottler.scala
@@ -1,8 +1,16 @@
 package com.workshop
 
+import java.util.concurrent.atomic.AtomicLong
+
 import scala.concurrent.duration.FiniteDuration
 
 
 class RollingWindowThrottler(durationWindow: FiniteDuration, max: Int) {
-  def tryAcquire(key: String): Boolean = true
+
+
+  private val invocationInfo = collection.mutable.HashMap.empty[String, AtomicLong]
+
+  def tryAcquire(key: String): Boolean = {
+    invocationInfo.getOrElseUpdate(key, new AtomicLong(0)).incrementAndGet() <= max
+  }
 }
--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -1,16 +1,30 @@
 package com.workshop
 
 
+import org.specs2.matcher.Scope
 import org.specs2.mutable.SpecWithJUnit
 import scala.concurrent.duration._
 
 
 class RollingWindowThrottlerTest extends SpecWithJUnit{
 
+  trait Context extends Scope{
+    val aThrottler = new RollingWindowThrottler(durationWindow = 1.minute, max = 1)
+    val anIp = "192.168.2.1"
+  }
+
   "RollingWindowThrottler" should {
-    "allow request within the sliding window" in {
-      val throttler = new RollingWindowThrottler(durationWindow = 1.minute, max = 1)
-      throttler.tryAcquire("192.168.2.1") must beTrue
+    "allow request within the sliding window" in new Context  {
+      aThrottler.tryAcquire(anIp) must beTrue
+    }
+    "throttle request which exceed the sliding window" in new Context {
+      aThrottler.tryAcquire(anIp) must beTrue
+      aThrottler.tryAcquire(anIp) must beFalse
+    }
+    "allow 2 requests with different key" in new Context {
+      val anotherIp = "192.168.10.10"
+      aThrottler.tryAcquire(anIp) must beTrue
+      aThrottler.tryAcquire(anotherIp) must beTrue
     }
   }
 

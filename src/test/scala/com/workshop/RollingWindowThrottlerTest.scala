3afc4c527bca75275dd4a023a5c0736ffe40cc79
kfirb <kfirb@wix.com>
2016-04-03 00:14:35 +0300

    first 2 tests, now it fail

--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -1,48 +1,25 @@
 package com.workshop
 
-
-import com.workshop.framework.FakeClock
-import org.specs2.matcher.{Matchers, Scope}
 import org.specs2.mutable.SpecWithJUnit
-import scala.concurrent.duration._
 
+import scala.util.Try
 
-class RollingWindowThrottlerTest extends SpecWithJUnit with Matchers{
-
-  trait Context extends Scope{
-    val clock = new FakeClock
-    val aThrottler = new RollingWindowThrottler(
-      durationWindow = 1.minute,
-      max = 1,
-      clock = clock)
-    val anIp = "192.168.2.1"
-  }
+class RollingWindowThrottlerTest extends SpecWithJUnit{
 
   "RollingWindowThrottler" should {
-    "allow request within the sliding window" in new Context  {
-      aThrottler.tryAcquire(anIp) must beAllowedRequest(anIp)
+    "Allow request" in {
+      val throttler = new RollingWindowThrottler()
+      throttler.tryAcquire("192.168.2.1") must beSuccessfulTry
     }
-    "throttle request which exceed the sliding window" in new Context {
-      aThrottler.tryAcquire(anIp) must beAllowedRequest(anIp)
-      aThrottler.tryAcquire(anIp) must beThrottledRequest(anIp)
-    }
-    "allow 2 requests with different key" in new Context {
-      val anotherIp = "192.168.10.10"
-      aThrottler.tryAcquire(anIp) must beAllowedRequest(anIp)
-      aThrottler.tryAcquire(anotherIp) must beAllowedRequest(anIp)
-    }
-    "re-allow request after ending of the sliding window" in new Context {
-      aThrottler.tryAcquire(anIp) must beAllowedRequest(anIp)
-      aThrottler.tryAcquire(anIp) must beThrottledRequest(anIp)
-      clock.age(2.minutes)
-      aThrottler.tryAcquire(anIp) must beAllowedRequest(anIp)
+    "Throttle request which exceeded the limit" in {
+      val throttler = new RollingWindowThrottler(max = 1)
+      throttler.tryAcquire("192.168.2.1") must beSuccessfulTry
+      throttler.tryAcquire("192.168.2.1") must beFailedTry
     }
   }
 
-  def beAllowedRequest(key: String) = beTrue ^^
-    { (b: Boolean) => b aka s"expected invocation key $key to be allowed but" }
+}
 
-  def beThrottledRequest(key: String) = beFalse ^^
-    { (b: Boolean) => b aka s"expected invocation key $key to be throttled but" }
-
+class RollingWindowThrottler(max: Int = 1) {
+  def tryAcquire(key: String): Try[Unit] = Try()
 }

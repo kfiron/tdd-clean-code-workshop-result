b6b7e53ea391fb22a3ac999a50397f47996ade91
kfirb <kfirb@wix.com>
2016-04-04 01:05:41 +0300

    3rd test

--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -20,15 +20,21 @@ class RollingWindowThrottlerTest extends SpecificationWithJUnit{
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
       aThrottler.tryAcquire(anIp) must beFailedTry
     }
+    "allow second request but with different key" in new ThrottlerScope{
+      val anotherIp = "200.200.200.1"
+      aThrottler.tryAcquire(anIp) must beSuccessfulTry
+      aThrottler.tryAcquire(anotherIp) must beSuccessfulTry
+    }
   }
 
 }
 
 class RollingWindowThrottler(max: Int = 1) {
 
-  val counter = Counter()
+  val invocations = scala.collection.mutable.HashMap.empty[String, Counter]
 
   def tryAcquire(key: String): Try[Unit] = {
+    val counter = invocations.getOrElseUpdate(key, Counter())
     if(counter.incrementAndGet <= max){
       Success()
     }else{

55f3f91c31573c4a9a90b3f098766813c236fe90
kfirb <kfirb@wix.com>
2016-04-05 22:45:58 +0300

    second test

--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -1,26 +1,30 @@
 package com.workshop
 
 import org.specs2.mutable.SpecificationWithJUnit
+import org.specs2.specification.Scope
 
 import scala.util.{Failure, Success, Try}
 
 class RollingWindowThrottlerTest extends SpecificationWithJUnit{
 
+  class RollingWindowScope extends Scope{
+    val anIp = "192.168.2.1"
+    val aThrottler = new RollingWindowThrottler(max = 1)
+  }
+
   "RollingWindowThrottler" should {
-    "Allow one request" in {
-      val throttler = new RollingWindowThrottler()
-      throttler.tryAcquire("192.168.2.1") must beSuccessfulTry
+    "Allow one request" in new RollingWindowScope{
+      aThrottler.tryAcquire(anIp) must beSuccessfulTry
     }
-    "Does not allow more then max requests" in {
-      val throttler = new RollingWindowThrottler(max = 1)
-      throttler.tryAcquire("192.168.2.1") must beSuccessfulTry
-      throttler.tryAcquire("192.168.2.1") must beFailedTry
+    "Does not allow more then max requests" in new RollingWindowScope{
+      aThrottler.tryAcquire("192.168.2.1") must beSuccessfulTry
+      aThrottler.tryAcquire("192.168.2.1") must beFailedTry
     }
   }
 
 }
 
-class RollingWindowThrottler(max: Int = 1) {
+class RollingWindowThrottler(max: Int) {
 
   val counter = Counter()
 

a14b91e76b2848f469cd87515b8539314d0f95a5
kfirb <kfirb@wix.com>
2016-04-05 23:08:36 +0300

    4th test

--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -1,19 +1,27 @@
 package com.workshop
 
+import java.time.{Instant, Clock}
+
+import com.workshop.framework.FakeClock
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
+import scala.concurrent.duration._
 
 import scala.util.{Failure, Success, Try}
 
 class RollingWindowThrottlerTest extends SpecificationWithJUnit{
 
   class RollingWindowScope extends Scope{
+    val clock = new FakeClock
     val anIp = "192.168.2.1"
-    val aThrottler = new RollingWindowThrottler(max = 1)
+    val aThrottler = new RollingWindowThrottler(
+      max = 1,
+      durationWindow = 1.minute,
+      clock = clock)
   }
 
   "RollingWindowThrottler" should {
-    "Allow one request" in new RollingWindowScope{
+   /* "Allow one request" in new RollingWindowScope{
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
     }
     "Throttle request that exceeded max requests" in new RollingWindowScope{
@@ -24,19 +32,32 @@ class RollingWindowThrottlerTest extends SpecificationWithJUnit{
       val anotherIp = "200.200.200.1"
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
       aThrottler.tryAcquire(anotherIp) must beSuccessfulTry
+    }*/
+    "Re-allow request after rolling window is ended" in new RollingWindowScope{
+      aThrottler.tryAcquire(anIp) must beSuccessfulTry
+      aThrottler.tryAcquire(anIp) must beFailedTry
+      clock.age(1.minute)
+      aThrottler.tryAcquire(anIp) must beSuccessfulTry
     }
   }
 
 }
 
-class RollingWindowThrottler(max: Int) {
+class RollingWindowThrottler(
+                              max: Int,
+                              durationWindow: FiniteDuration,
+                              clock: Clock) {
 
   val counter = Counter()
-  val invocations = scala.collection.mutable.HashMap.empty[String, Counter]
+  val invocations = scala.collection.mutable.HashMap.empty[String, Invocation]
 
   def tryAcquire(key: String): Try[Unit] = {
-    val count = invocations.getOrElseUpdate(key, Counter()).incrementAndGet
-    if(count <= max) {
+    val invocation = invocations.getOrElseUpdate(key, Invocation(clock.instant(), Counter()))
+
+    if((clock.instant().toEpochMilli - invocation.timestamp.toEpochMilli) >= durationWindow.toMillis){
+      invocations -= key
+      tryAcquire(key)
+    } else if(invocation.counter.incrementAndGet <= max) {
       Success()
     }else{
       Failure(new Exception)
@@ -50,3 +71,5 @@ case class Counter(var count: Int = 0){
     count
   }
 }
+
+case class Invocation(timestamp: Instant, counter: Counter)

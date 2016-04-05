56d81737ca957002e4bfc6713d78fbf35056cb6a
kfirb <kfirb@wix.com>
2016-04-05 23:21:15 +0300

    introduce guava cache

--- src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
+++ src/test/scala/com/workshop/RollingWindowThrottlerTest.scala
@@ -1,7 +1,10 @@
 package com.workshop
 
 import java.time.{Instant, Clock}
+import java.util.concurrent.TimeUnit
 
+import com.google.common.base.Ticker
+import com.google.common.cache.{CacheLoader, CacheBuilder, LoadingCache}
 import com.workshop.framework.FakeClock
 import org.specs2.mutable.SpecificationWithJUnit
 import org.specs2.specification.Scope
@@ -9,9 +12,9 @@ import scala.concurrent.duration._
 
 import scala.util.{Failure, Success, Try}
 
-class RollingWindowThrottlerTest extends SpecificationWithJUnit{
+class RollingWindowThrottlerTest extends SpecificationWithJUnit {
 
-  class RollingWindowScope extends Scope{
+  class RollingWindowScope extends Scope {
     val clock = new FakeClock
     val anIp = "192.168.2.1"
     val aThrottler = new RollingWindowThrottler(
@@ -21,19 +24,19 @@ class RollingWindowThrottlerTest extends SpecificationWithJUnit{
   }
 
   "RollingWindowThrottler" should {
-   /* "Allow one request" in new RollingWindowScope{
+    "Allow one request" in new RollingWindowScope {
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
     }
-    "Throttle request that exceeded max requests" in new RollingWindowScope{
+    "Throttle request that exceeded max requests" in new RollingWindowScope {
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
       aThrottler.tryAcquire(anIp) must beFailedTry
     }
-    "Allow second request but with different key" in new RollingWindowScope{
+    "Allow second request but with different key" in new RollingWindowScope {
       val anotherIp = "200.200.200.1"
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
       aThrottler.tryAcquire(anotherIp) must beSuccessfulTry
-    }*/
-    "Re-allow request after rolling window is ended" in new RollingWindowScope{
+    }
+    "Re-allow request after rolling window is ended" in new RollingWindowScope {
       aThrottler.tryAcquire(anIp) must beSuccessfulTry
       aThrottler.tryAcquire(anIp) must beFailedTry
       clock.age(1.minute)
@@ -48,45 +51,36 @@ class RollingWindowThrottler(
                               durationWindow: FiniteDuration,
                               clock: Clock) {
 
-  val counter = Counter()
-  val invocations = scala.collection.mutable.HashMap.empty[String, Invocation]
+
+  val invocations: LoadingCache[String, Counter] = CacheBuilder.newBuilder()
+    .expireAfterWrite(durationWindow.toMillis, TimeUnit.MILLISECONDS)
+    .ticker(throttlerTicker())
+    .build(defaultCounter())
 
   def tryAcquire(key: String): Try[Unit] = {
-    val invocation = invocations.getOrElseUpdate(key, newInvocation)
-    (expirationHandler orElse throttlerHandler)(key -> invocation)
-  }
-
-  val expirationHandler: PartialFunction[(String, Invocation), Try[Unit]] = {
-    case (key, invocation) if expires(invocation) => {
-      invocations -= key
-      tryAcquire(key)
+    val count = invocations.get(key).incrementAndGet
+    if (count <= max) {
+      Success()
+    } else {
+      Failure(new Exception)
     }
   }
 
-  val throttlerHandler: PartialFunction[(String, Invocation), Try[Unit]] = {
-    case (key, invocation) => {
-      if(invocation.counter.incrementAndGet <= max){
-        Success()
-      }else {
-        Failure(new Exception)
-      }
-    }
+  def defaultCounter(): CacheLoader[String, Counter] = new CacheLoader[String, Counter] {
+    override def load(k: String): Counter = Counter()
   }
 
-  def newInvocation: Invocation =
-    Invocation(clock.instant(), Counter())
-
-
-  def expires(invocation: Invocation): Boolean =
-    (clock.instant().toEpochMilli - invocation.timestamp.toEpochMilli) >= durationWindow.toMillis
+  def throttlerTicker(): Ticker = new Ticker {
+    override def read(): Long = TimeUnit.MILLISECONDS.toNanos(clock.instant().toEpochMilli)
+  }
 
 }
 
-case class Counter(var count: Int = 0){
-  def incrementAndGet : Int = {
-    count +=1
+case class Counter(var count: Int = 0) {
+  def incrementAndGet: Int = {
+    count += 1
     count
   }
 }
 
-case class Invocation(timestamp: Instant, counter: Counter)
+
